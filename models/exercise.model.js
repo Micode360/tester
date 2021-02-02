4be9c89b69f2354a3fc449855b17065efab8407d
KING <abayemiracle@gmail.com>
2021-02-02 12:40:19 +0100

    new update

---
+++ models/exercise.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema ({
+    username: {
+        type: String, 
+        required: true 
+    },
+    count: {
+        type: Number, 
+        required: true 
+    },
+    exercise: [{
+        description: {type: String, required: true },
+        duration: {type: Number, required: true },
+        date: {type: String, required: true }
+    }]
+},{
+    timestamps:true,
+});
+
+
+
+
+const exerciseModel = mongoose.model('exerciseModel', exerciseSchema);
+
+module.exports = exerciseModel;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const multer = require("multer");
 const upload = multer();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const moment = require('moment');
+const exerciseModel = require("./models/exercise.model");
 
 require('dotenv').config();
 
@@ -22,8 +24,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// // let's make json a bit cleaner
-// app.set("json spaces", 2);
+
 
 //Send the HTML
 app.use(express.static("public"));
@@ -33,98 +34,76 @@ app.get("/", (req, res) => {
 
 //Import model
 let user = require("./schema.js").user;
-// functions
-function isValidDate(d) {
-  return d instanceof Date && !isNaN(d);
-}
 
-//  /api/exercise/new-user  code
-app.post("/api/exercise/new-user", (req, res, next) => {
+app.post('/api/exercise/new-user', (req, res) => {
   let username = req.body.username;
 
-  if (username) {
-    //check if name is entered in field
-    let userAdd = { username: username, count: 0, log: [] };
-    user.findOne({ username: userAdd.username }, (err, data) => {
-      if (err) next(err);
-      if (data) {
-        res.send("Username is already taken.");
-      } else {
-        user.create(userAdd, (err, data) => {
-          if (err) next(err);
-          res.json({ username: data.username, _id: data._id });
-        });
-      }
-    });
-  } else {
-    res.send("Please provide a username");
-  }
-});
+  exerciseModel.findOne({ username: username }, (err, name) => {
+    if (err) return err;
 
-//  /api/exercise/add  code
-app.post("/api/exercise/add", upload.none(), (req, res, next) => {
-  console.log("Status: " + res.statusCode);
-  console.log("Header: " + JSON.stringify(req.headers));
-  var contype = req.headers["content-type"];
-  console.log(contype);
+    if (name) res.send('Username already taken')
+    else {
+      const exerciseData = new exerciseModel({
+        username,
+        count: 0,
+        exercise: []
+      });
 
-  let userId = req.body.userId;
-  let description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date ? new Date(req.body.date) : new Date();
-  console.log(
-    "userId:",
-    req.body.userId,
-    "description:",
-    req.body.description,
-    "duration:",
-    req.body.duration,
-    "date:",
-    req.body.date
-  );
-  if (contype.split(";", 1) == "multipart/form-data") {
-    console.log("Headers:====\n", req.headers);
-    console.log("body========\n", req.body);
-    console.log("Request Object========\n", req);
-  }
-
-  let exerciseData;
-
-  if (userId && description && duration) {
-    user.findById(userId, function(err, data) {
-      if (err) {
-        console.log(err);
-        next(err);
-      }
-      if (data) {
-        console.log("user Data found:", data);
-        console.log("found User and adding exercise");
-        data.count = data.count + 1;
-        let additionExercise = {
-          description: description,
-          duration: duration,
-          date: date.toDateString()
-        };
-        data.log.push(additionExercise);
-        data.save((err, data) => {
-          if (err) console.log(err);
-          exerciseData = {
+      exerciseData.save()
+        .then((data) => {
+          res.json({
             username: data.username,
-            _id: data._id,
-            description: description,
-            duration: duration,
-            date: date.toDateString()
-          };
-          console.log(exerciseData);
-          console.log(additionExercise);
-          res.json(exerciseData);
-        });
-      }
-    });
-  } else {
-    res.send("Please fill in all required fields.");
-  }
+            _id: data._id
+          })
+        }).catch(err => res.status(400).json('Error' + err));
+    }
+  });
+
 });
+
+app.get('/api/exercise/users', (req, res) => {
+  exerciseModel.find()
+    .then((data) => {
+      res.send(data);
+    });
+});
+
+app.post('/api/exercise/add', (req, res) => {
+  const exerciseForm = req.body;
+
+  let momentDate = moment().format('ddd MMM DD YYYY').toString()
+
+  if(exerciseForm.date === '') exerciseForm.date = momentDate;
+  else exerciseForm.date =  moment(exerciseForm.date).format('ddd MMM DD YYYY').toString();
+  
+
+  const exerciseData = {
+    description: exerciseForm.description,
+    duration: exerciseForm.duration,
+    date: exerciseForm.date
+  }
+
+
+
+  exerciseModel.findOneAndUpdate({ _id: exerciseForm.userId },{ new: true }, (err, person)=>{
+      if(err) return err;
+      person.exercise = exerciseData;
+      person.save((err, update) =>{
+      if(err) return console.log(err);
+
+        res.json(
+          {
+            username:update.username,
+            description:update.exercise[0].description,
+            duration:Number(update.exercise[0].duration),
+            _id: update._id,
+            date: update.exercise[0].date
+        })
+
+      })
+     })
+})
+
 
 app.get("/api/exercise/log", function(req, res, next) {
   let userId = req.query.userId;
@@ -136,10 +115,10 @@ app.get("/api/exercise/log", function(req, res, next) {
     const limitOptions = {};
     if (limit) limitOptions.limit = limit;
 
-    user
+    exerciseModel
       .findById(userId)
       .populate({
-        path: "log",
+        path: "exercise",
         match: {},
         select: "-_id",
         options: limitOptions
@@ -154,7 +133,7 @@ app.get("/api/exercise/log", function(req, res, next) {
           };
           if (from) displayData.from = from.toDateString();
           if (to) displayData.to = to.toDateString();
-          displayData.log = data.log.filter(item => {
+          displayData.log = data.exercise.filter(item => {
             if (from && to) {
               return item.date >= from && item.date <= to;
             } else if (from) {
@@ -178,52 +157,7 @@ app.get("/api/exercise/log", function(req, res, next) {
   }
 });
 
-app.get("/api/exercise/users", function(req, res) {
-  user.find({}, function(err, data) {
-    if (err) err;
-    let obj = data.map(item => {
-      return `username:${item.username}, _id:${item._id}`;
-    });
-    res.json(obj);
-  });
-});
 
-app.get("/api/exercise/users", function(req, res) {
-  console.log("loading///");
-  user.find({}, function(err, data) {
-    if (err) err;
-    let obj = data.map(item => {
-      return `username:${item.username}, _id:${item._id}`;
-    });
-    res.json(obj);
-  });
-});
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" });
-});
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage;
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400; // bad request
-    const keys = Object.keys(err.errors);
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message;
-  } else {
-    // generic or custom error
-    errCode = err.status || 500;
-    errMessage = err.message || "Internal Server Error";
-  }
-  res
-    .status(errCode)
-    .type("txt")
-    .send(errMessage);
-});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
